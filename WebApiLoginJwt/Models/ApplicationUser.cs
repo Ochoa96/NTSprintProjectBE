8fdcf0d7730a0845a9527bfeeee28ce57d5ff1b5
Omar Ochoa Garcia <oscamar1101@gmail.com>
2021-06-05 13:43:06 -0500

    -Creada Base de datos -Agregado DbContext de la base de datos -Configurando la clase Startup para elmuso de la base de datos y que esta sea en memoria

---
+++ WebApiLoginJwt/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLoginJwt.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        //clase vacia q heredea de IdentityUser y
+        //representa a un usuario de la aplicacion
+    }
+}
