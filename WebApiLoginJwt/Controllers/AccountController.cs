87dd6ec04b325c93c2436ea5079a9930ca999561
Omar Ochoa Garcia <oscamar1101@gmail.com>
2021-06-05 14:28:30 -0500

    -Creado controlador del Api -Configurando el constructor del controlador para el uso de Identity -Creado Modelo del usuario(UserInfo) -Implementado metodo(POST) Create para registrar un usuario -Implementado metodo(POST) Login para loguear un usuario

---
+++ WebApiLoginJwt/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApiLoginJwt.Models;
+
+namespace WebApiLoginJwt.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _conf;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            this._conf = configuration;
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] UserInfo model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.user,
+                    Email = model.email
+                };
+                var result = await _userManager.CreateAsync(user, model.password);
+                if (result.Succeeded)
+                    return BuildToken(model);
+                else
+                    return BadRequest(result.Errors);
+            }
+            else
+                return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] UserInfo model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.user, model.password, false, false);
+                if (result.Succeeded)
+                    return BuildToken(model);
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                    return BadRequest(ModelState);
+                }
+            }
+            else
+                return BadRequest(ModelState);
+        }
+    }
+}
---
+++ WebApiLoginJwt/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLoginJwt.Models
+{
+    public class UserInfo
+    {
+        public string user { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
